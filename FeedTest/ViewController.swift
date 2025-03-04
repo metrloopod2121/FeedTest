972af7f29b759a3d48c3ffec4b4b6bfd3816ca20
Matvey <metrloopod2121@mail.ru>
2025-03-04 18:53:12 +0300

    Initial Commit

---
+++ FeedTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeedTest
+//
+//  Created by 𝕄𝕒𝕥𝕧𝕖𝕪 ℙ𝕠𝕕𝕘𝕠𝕣𝕟𝕚𝕪 on 04.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FeedTestTests/FeedTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  FeedTestTests.swift
+//  FeedTestTests
+//
+//  Created by 𝕄𝕒𝕥𝕧𝕖𝕪 ℙ𝕠𝕕𝕘𝕠𝕣𝕟𝕚𝕪 on 04.03.2025.
+//
+
+import Testing
+@testable import FeedTest
+
+struct FeedTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
